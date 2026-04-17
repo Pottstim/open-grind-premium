942303df5e86e379edecd4932e881d61fb8fef45
Viktor Shchelochkov <hi@hloth.dev>
2026-04-17 14:59:09 +0200

    Add auth to Rust lib (#20)
    Reviewed-on: https://git.hloth.dev/hloth/open-grind/pulls/20
    Co-authored-by: Viktor Shchelochkov <hi@hloth.dev>
    Co-committed-by: Viktor Shchelochkov <hi@hloth.dev>

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,26 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-	format!("Hello, {}! You've been greeted from Rust!!", name)
-}
+mod api;
+mod error;
+mod state;
+mod storage;
+
+use crate::state::AppState;
+use api::client::GrindrClient;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-	tauri::Builder
-		::default()
-		.plugin(tauri_plugin_opener::init())
-		.invoke_handler(tauri::generate_handler![greet])
-		.run(tauri::generate_context!())
-		.expect("error while running tauri application");
+    storage::init_keyring();
+
+    let client = GrindrClient::new().ok();
+
+    tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_opener::init())
+        .manage(AppState { client })
+        .invoke_handler(tauri::generate_handler![
+            api::auth::login,
+            api::auth::refresh_token,
+            api::auth::logout,
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
