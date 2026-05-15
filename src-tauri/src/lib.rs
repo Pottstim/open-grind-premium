6d2297eccc9f87cb990f300c1f268ea25308f4db
Viktor Shchelochkov <hi@hloth.dev>
2026-05-16 00:21:50 +0200

    Add CrabNebula DevTools plugin to Rust for mobile debugging (enabled in dev builds only)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -12,10 +12,20 @@ use api::client::GrindrClient;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+	#[cfg(debug_assertions)]
+	let devtools = tauri_plugin_devtools::init();
+
     let (ws_tx, ws_rx) = mpsc::channel(64);
     let auth_notify = Arc::new(Notify::new());
 
-    tauri::Builder::default()
+    let mut builder = tauri::Builder::default();
+
+	#[cfg(debug_assertions)]
+    {
+        builder = builder.plugin(devtools);
+    }
+
+	builder
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_geolocation::init())
